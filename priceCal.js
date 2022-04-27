241971dda9b15d1ef800ec744679c823fe36f8cd
nut <2010511101027@live4.utcc.ac.th>
2022-04-27 16:23:44 +0700

    fix function price booked

--- priceCal.js
+++ priceCal.js
@@ -2,8 +2,15 @@ function priceCalculate() {
   let ticknum = (document.getElementById("ticknum").value).trim();
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  
-  return 0;
+  let price=0;
+  if (eventId==1){
+    price=91.25
+  }else if (eventId==2) {
+    price=78.88
+  }else if (eventId==3) {
+    price=83.12
+  }else if (eventId==4) {
+    price=100
+  }
+  return ticknum*price;
 }
-
-
